f95d096576b93eb1ad5809b46cf9b1fb61839579
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-01-11 18:35:47 -0300

    Added initial configuration

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ivan Romero on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Ivan Romero on 11/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    // MARK: - FUNCTION
+    // MARK: - CONTENT
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
