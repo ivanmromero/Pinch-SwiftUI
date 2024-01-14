e3a3af4e17a71aa6feca1ae2d55d6c5bdc3ec117
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-01-14 16:22:57 -0300

    Added Drag Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,9 +11,15 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     // MARK: - FUNCTION
+    func resetImageState() {
+        withAnimation(.spring) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     // MARK: - CONTENT
-    
     var body: some View {
         NavigationStack {
             ZStack(content: {
@@ -21,11 +27,12 @@ struct ContentView: View {
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    // Corner radius deprecated in future, replace with 👇🏻
+                // Corner radius deprecated in future, replace with 👇🏻
                     .clipShape(.rect(cornerRadius: 10))
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 //MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
@@ -35,10 +42,25 @@ struct ContentView: View {
                             }
                         } else {
                             withAnimation(.spring) {
-                                imageScale = 1
+                                resetImageState()
                             }
                         }
                     })
+                //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
+                
             })
             .navigationTitle("Pich & Zoom")
             .navigationBarTitleDisplayMode(.inline)
